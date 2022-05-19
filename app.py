0c8bd2c995b1e4cd03eb6cf3977d786b41e96cc7
Noble Koshy <koshy009@umn.edu>
2022-05-19 18:44:28 -0500

    Add files via upload

---
+++ app.py
@@ -0,0 +1,46 @@
+from flask import Flask, render_template, json, redirect
+from flask_mysqldb import MySQL
+
+#added these imports:
+#from flask import request
+#import MySQLdb
+
+import os
+
+app = Flask(__name__)
+
+app.config['MYSQL_HOST'] = 'classmysql.engr.oregonstate.edu'
+app.config['MYSQL_USER'] = 'cs340_koshyn'
+app.config['MYSQL_PASSWORD'] = '2356' #last 4 of onid
+app.config['MYSQL_DB'] = 'cs340_koshyn'
+app.config['MYSQL_CURSORCLASS'] = "DictCursor"
+
+
+mysql = MySQL(app)
+
+
+# Routes
+@app.route('/')
+def root():
+    query = "SELECT * FROM diagnostic;"
+    query1 = 'DROP TABLE IF EXISTS diagnostic;';
+    query2 = 'CREATE TABLE diagnostic(id INT PRIMARY KEY AUTO_INCREMENT, text VARCHAR(255) NOT NULL);';
+    query3 = 'INSERT INTO diagnostic (text) VALUES ("MySQL is working!")';
+    query4 = 'SELECT * FROM diagnostic;';
+    cur = mysql.connection.cursor()
+    cur.execute(query1)
+    cur.execute(query2)
+    cur.execute(query3)
+    cur.execute(query4)
+    results = cur.fetchall()
+
+    # return results[0]
+    return render_template("index.j2")
+
+
+# Listener
+if __name__ == "__main__":
+
+    #Start the app on port 3000, it will be different once hosted
+    app.run(port=19956, debug=True)
+
